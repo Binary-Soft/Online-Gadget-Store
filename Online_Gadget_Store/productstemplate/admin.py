9684555bd65a57377a3c1192b560fdf821998923
Partho Debnath <parthodebnath28@gmail.com>
2022-06-26 22:18:26 +0600

    updated

--- Online_Gadget_Store/productstemplate/admin.py
+++ Online_Gadget_Store/productstemplate/admin.py
@@ -17,5 +17,11 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['category', 'brand_name', 'product_name']
     list_filter = ['category', 'brand_name', ]
 
+
 admin.site.register(Order)
-admin.site.register(WishList)
+
+
+@admin.register(WishList)
+class WishListAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'quantity']
+    list_filter = ['user', 'product']
